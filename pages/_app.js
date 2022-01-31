87f116a00c2ba7a9f8bd810eaa4a1bbd9de56753
raushan <moraush@gmail.com>
2022-01-31 23:55:12 +1100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import Head from 'next/Head';
+import NProgress from 'nprogress';
+import {ChakraProvider} from '@chakra-ui/react';
+import Router from 'next/router'
+
+import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+
+    </Head>
+    <ChakraProvider>
+      <Layout>
+        <Component {...pageProps}/>
+      </Layout>
+    </ChakraProvider>
+    
+    </>
+  )
 }
 
 export default MyApp
